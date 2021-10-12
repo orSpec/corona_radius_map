7cdf54765100f537e5c37882cf69aeaad2c35f9d
orSpec <deniswolf@t-online.de>
2021-10-12 19:53:49 +0200

    added config "SEND_FILE_MAX_AGE_DEFAULT"

--- flask_corona_radius.py
+++ flask_corona_radius.py
@@ -3,7 +3,8 @@ import folium
 from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -11,6 +12,7 @@ def home():
         result = request.form['number']
 
         location = geocode(result)
+        print(location)
         if location != None:
             createRadiusMap(location)
             latitude = location.latitude
@@ -21,6 +23,7 @@ def home():
     else:
         result = None
         coords = None
+
     return render_template('corona_radius.html', result=result, coords=coords)
 
 @app.route("/maps/map_radius.html")
@@ -42,7 +45,6 @@ def createRadiusMap(location):
         folium_map = folium.Map(location=location, zoom_start=10)
         folium.Circle(location, radius=15000).add_to(folium_map)
         folium_map.save("./maps/map_radius.html")
-
 
 if __name__ == '__main__':
     app.run()
